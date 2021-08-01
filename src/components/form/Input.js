25e43caff19db3e8a2ecd165c2c558c14d4410ce
AJ <79541631+AJKilmurray@users.noreply.github.com>
2021-08-02 00:05:13 +0100

    creation

---
+++ src/components/form/Input.js
@@ -0,0 +1,12 @@
+import TextField from "@material-ui/core/TextField";
+
+function Input() {
+  return (
+    <>
+      <TextField id="input" label="Currency" />
+      <TextField id="amount" label="Amount" />
+    </>
+  );
+}
+
+export default Input;
---
+++ src/components/form/SearchButton.js
@@ -0,0 +1,13 @@
+import Button from "@material-ui/core/Button";
+
+function SearchButton() {
+  return (
+    <>
+      <Button variant="contained" color="secondary" type="submit">
+        Search
+      </Button>
+    </>
+  );
+}
+
+export default SearchButton;
